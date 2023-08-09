6d7056394ea9718400a0d0ca80724eb06324b981
ouafa-wafa <wfoufou643@gmail.com>
2023-08-09 19:15:57 +0200

    finish

--- project-7-accordion/index.js
+++ project-7-accordion/index.js
@@ -1,8 +1,11 @@
 const buttons=document.querySelectorAll(".plus");
 buttons.forEach(item => {
     item.addEventListener("click",(eo) => {
+        //bring the class content of our para
        const content=eo.target.parentElement.parentElement.querySelector(".content");
+       //change the opacity and the hight =>we would like to show the content
        content.classList.toggle("active")
+       //toggle between the + and -
        item.classList.toggle("hide-plus")
        if(item.classList.contains('hide-plus')){
         //contains verify if the class hide plus exist or not     ^-^
@@ -15,3 +18,5 @@ buttons.forEach(item => {
       
     })
 });
+
+
